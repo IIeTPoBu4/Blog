169cea50ca65f6798d0775016dcaba0cf728250f
Egor <egordreshel@gmail.com>
2018-10-25 11:40:15 +0300

    Creating category for article

---
+++ models/Article.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\data\Pagination;
+use yii\helpers\ArrayHelper;
+/**
+ * This is the model class for table "{{%article}}".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property string $content
+ * @property string $date
+ * @property string $image
+ * @property int $viewed
+ * @property int $user_id
+ * @property int $status
+ * @property int $category_id
+ *
+ * @property ArticleTag[] $articleTags
+ * @property Comment[] $comments
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%article}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title','description', 'content'], 'string'],
+            [['title'],'required'],
+            [['date'], 'date','format'=>'php:Y-m-d'],
+            [['title', 'image'], 'string', 'max' => 255],
+            [['date'], 'default', 'value' => date('Y-m-d')],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'date' => 'Date',
+            'image' => 'Image',
+            'viewed' => 'Viewed',
+            'user_id' => 'User ID',
+            'status' => 'Status',
+            'category_id' => 'Category ID',
+        ];
+    }
+
+    public function saveImage($filename)
+    {
+        $this->image = $filename;
+        return $this->save(false);
+    }
+
+    public function getImage()
+    {
+
+        //Либо так:
+
+        /*if($this->image)
+        {
+            return '/uploads' . $this->image;
+        }
+        return '/no-image.png';*/
+
+        //Либо так:
+        return ($this->image) ? '/uploads/' . $this->image : '/no-image.png';
+        
+    }
+
+    public function deleteImage()
+    {
+        $imageUploadModel = new ImageUpload();
+        $imageUploadModel->deleteCurrentImage($this->image);
+
+    }
+
+    public function beforeDelete()
+    {
+        $this->deleteImage();
+        return parent::beforeDelete();
+    }
+
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
+    }
+
+    public function saveCategory($category_id)
+    {
+        $category = Category::findOne($category_id);
+        if($category != null)
+        {
+            $this->link('category', $category);
+            return true;
+        }
+        
+    }
+}
+  
